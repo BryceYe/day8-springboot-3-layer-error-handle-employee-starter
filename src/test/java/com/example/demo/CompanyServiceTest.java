6e3b500d1514cce35b057cad76de963cb3364c8f
kestrel <1812370648@qq.com>
2025-09-10 21:51:04 +0800

    test: add CompanyService unit tests

---
+++ src/test/java/com/example/demo/CompanyServiceTest.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+import com.example.demo.entity.Company;
+import com.example.demo.exception.UpdateCompanyException;
+import com.example.demo.repository.CompanyRepository;
+import com.example.demo.service.impl.CompanyServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class CompanyServiceTest {
+    @InjectMocks
+    private CompanyServiceImpl companyServiceImpl;
+
+    @Mock
+    private CompanyRepository companyRepository;
+
+    @Test
+    void should_exception_when_create_a_company(){
+        Company company = new Company(null, "huawei");
+        when(companyRepository.createCompany(any(Company.class))).thenReturn(company);
+        Company exception = companyServiceImpl.createCompany(company);
+        assertEquals(exception.getName(), company.getName());
+    }
+
+    @Test
+    void should_create_company_with_default_true_when_create_a_company(){
+        Company company = new Company(null, "huawei");
+        when(companyRepository.createCompany(any(Company.class))).thenReturn(company);
+        companyServiceImpl.createCompany(company);
+        assertTrue(company.isActive());
+    }
+
+    @Test
+    void should_set_company_active_false_when_delete_a_company(){
+        Company company = new Company(1, "huawei");
+        assertTrue(company.isActive());
+        when(companyRepository.getCompanyById(1)).thenReturn(company);
+        companyServiceImpl.deleteCompany(1);
+        verify(companyRepository).updateCompany(eq(1),argThat(company1 -> !company1.isActive()));
+    }
+
+    @Test
+    void should_return_error_message_when_update_active_false_company(){
+        Company company = new Company(1, "huawei");
+        assertTrue(company.isActive());
+        when(companyRepository.getCompanyById(1)).thenReturn(company);
+        companyServiceImpl.deleteCompany(1);
+
+        assertThrows(UpdateCompanyException.class, () -> companyServiceImpl.updateCompany(1,new Company(1, "huawei")));
+    }
+}
